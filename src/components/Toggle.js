341196729acd8ad89afd922e95453e0d27514e45
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-04-26 19:07:08 -0400

    commit tree da4f45be6c854ddad88879321f1bbf698e2c029a

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [state, setState] = useState('OFF')
+
+  const color = state==='ON' ? 'red' : 'white';
+
+  const onClick = () => {
+    if(state === 'OFF'){
+      setState('ON')
+    }else{
+      setState('OFF')
+    }
+    /*setState((state)=>{
+      if(state === 'OFF'){
+        setState('ON')
+      }else{
+        setState('OFF')
+      }
+    })*/
+  }
+
+  return <button 
+            style={{background: color}}
+            onClick={onClick}
+            >{state}</button>;
 }
 
 export default Toggle;
